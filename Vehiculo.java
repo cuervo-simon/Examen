3290bd0347814a48fffee17f7d3b81a8efc467ae
cuervo-simon <cuervosimon90@gmail.com>
2025-05-03 10:54:21 -0500

    Create Vehiculo.java

---
+++ Vehiculo.java
@@ -0,0 +1,15 @@
+package Automovil;
+
+public class Vehiculo { void arrancar ()
+	
+   {System.out.println("Vehiculo arrancando"); } }
+
+class Coche extends Vehiculo { void arrancar ()
+	
+	{System.out.println ("Coche arrancando"); } }
+
+class Moto extends Vehiculo { void arrancar ()
+
+	{System.out.println ("Moto arrancando"); } 
+
+}
